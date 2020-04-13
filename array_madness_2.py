4228220f67d0212b745d9fce472ff8a845324daa
csfranze <craig.s.franze@gmail.com>
2020-04-12 23:43:03 -0400

    My solution to the Speed Code 2 Array Madness kata.

---
+++ array_madness_2.py
@@ -0,0 +1,11 @@
+# Objective
+
+# Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+# E.g.
+
+# array_madness([4, 5, 6], [1, 2, 3]) => True #because 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+
+
+def array_madness(a, b):
+    return sum([i**2 for i in a]) > sum([j**3 for j in b])
